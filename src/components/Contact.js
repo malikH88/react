0c37a776e7edb35847f0e2a9389159c0aa9c1663
malikH88 <malik.hyamine@gmail.com>
2020-04-13 01:42:56 +0200

    Manipulation des props et plusieurs test...COOL

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,21 @@
 import React from "react";
 import"./Contact.css";
 
-function Contact({ name, avatar, online }) {
+function Contact({ name, avatar, online, life, power, speed, combo, special }) {
   return (
     <div className="Contact">
       <img className="avatar" src={avatar} alt={name} />
       <div>
-        <p className="name">{name}</p>
+        <h1 className="name">{name}</h1>
         <div className="status">
           <div className={online ? "status-online" : "status-offline"} />
-          <p className="status-text">{online ? "online" : "offline"}</p>
+          <p className="status-text">{online ? "Mastered" : "Normal"}</p>
         </div>
+        <p>Life: {life}</p>
+        <p>Power: {power}</p>
+        <p>Speed: {speed}</p>
+        <p>Combo: {combo}</p>
+        <p>Special: {special}</p>
       </div>
     </div>
   );
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,36 +3,69 @@ import Contact from './Contact';
 
   const users = [
     {
-      name: 'Robert Reye',
-      avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
-      online: false
+      name: 'Ryu',
+      avatar: 'https://vignette.wikia.nocookie.net/streetfighter/images/c/ce/IMG_0506.png/revision/latest?cb=20170310152338',
+      online: false,
+      life: '4',
+      power: '4',
+      speed: '3',
+      combo: '2',
+      special: '3'
+      
     },
     {
-      name: 'Nellie Caldwell',
+      name: 'Ken Master',
       avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
-      online: true
+      online: true,
+      life: '3',
+      power: '4',
+      speed: '3',
+      combo: '3',
+      special: '3'
     },
     {
-      name: 'Vernon Mason',
+      name: 'Colonel Guile',
       avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
-      online: true
+      online: true,
+      life: '4',
+      power: '3',
+      speed: '3',
+      combo: '2',
+      special: '3'
     },
     {
-      name: 'Erica Hunt',
+      name: 'Chun-li',
       avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
-      online: false
+      online: false,
+      life: '2',
+      power: '3',
+      speed: '4',
+      combo: '4',
+      special: '3'
     },
     {
-      name: 'Juanita Phillips',
+      name: 'Akuma',
       avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
-      online: true
+      online: true,
+      life: '4',
+      power: '5',
+      speed: '3',
+      combo: '3',
+      special: '5'
     }
   ];
 
 
 //on parcours a l'aide de map le tableaux USER et on affiche chaque element de ce tableaux a la manieres de contact mais avec les valeurs de cet élément.
 const ContactList = () =>
-  users.map(props => <Contact name = {props.name} avatar = {props.avatar} online = {props.online}/>);
+  users.map(props => <Contact name = {props.name} 
+                              avatar = {props.avatar} 
+                              online = {props.online} 
+                              life = {props.life} 
+                              power = {props.power} 
+                              speed = {props.speed}
+                              combo = {props.combo}
+                              special = {props.special}/>);
 
 
 
